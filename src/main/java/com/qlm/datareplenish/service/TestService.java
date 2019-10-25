63898ab8f8029e863448f1e36de89c563554b73b
刘常林 <751969316@qq.com>
2019-10-25 15:50:32 +0800

    test

---
+++ src/main/java/com/qlm/datareplenish/service/TestService.java
@@ -0,0 +1,11 @@
+package com.qlm.datareplenish.service;
+
+/**
+ * @author liuchanglin
+ * @version 1.0
+ * @ClassName: TestService
+ * @Description: TODO(这里用一句话描述这个类的作用)
+ * @date 2019/10/25 3:50 下午
+ */
+public interface TestService {
+}
