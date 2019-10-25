81aca17b0dc8356e684da3ff5f7e60b33fe202f3
刘常林 <751969316@qq.com>
2019-10-25 15:01:02 +0800

    package fix

---
+++ src/main/java/com/qlm/datareplenish/task/SaticScheduleTask.java
@@ -0,0 +1,31 @@
+package com.qlm.datareplenish.task;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+/**
+ * @author liuchanglin
+ * @version 1.0
+ * @ClassName: SaticScheduleTask
+ * @Description: TODO(这里用一句话描述这个类的作用)
+ * @date 2019/10/25 9:40 上午
+ */
+@Component
+@Configuration      //1.主要用于标记配置类，兼备Component的效果。
+@EnableScheduling   // 2.开启定时任务
+@Slf4j
+public class SaticScheduleTask {
+    //3.添加定时任务
+    @Scheduled(cron = "0/5 * * * * ?")
+    //或直接指定时间间隔，例如：5秒
+    //@Scheduled(fixedRate=5000)
+    private void configureTasks() {
+        System.err.println("执行静态定时任务时间: " + LocalDateTime.now());
+        log.info("执行静态定时任务时间: " + LocalDateTime.now());
+    }
+}
